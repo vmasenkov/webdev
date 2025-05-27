17ec6f35fc82265ddf156b2b82be60c6ff5021b4
Vladyslav Masenkov <vladyslav.masenkov@gmail.com>
2025-05-27 18:40:36 +0300

    Add initial HTML structure for hotel room booking application
    - Created lab8.php with a complete HTML5 template
    - Included Bootstrap and Font Awesome for styling
    - Added jQuery and DayPilot libraries for functionality
    - Set up header, main, and footer sections with relevant content

---
+++ src/lab8.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>HTML5 Бронювання кімнат в готелі (JavaScript/PHP/MySQL)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style-lab8.css" type="text/css" />
+    <!-- допоміжні бібліотеки -->
+    <script src="js/jquery-3.7.1.min.js" type="text/javascript"></script>
+    <!-- бібліотека daypilot -->
+    <script src="js/daypilot-all.min.js" type="text/javascript"></script>
+</head>
+<body>
+  <header>
+    <div class="bg-help">
+      <div class="inBox">
+        <h1 id="logo">HTML5 Бронювання кімнат в готелі (JavaScript/PHP)</h1>
+        <p id="claim">AJAX'овий Календар-застосунок з JavaScript/HTML5/jQuery</p>
+        <hr class="hidden" />
+      </div>
+    </div>
+  </header>
+  <main>
+    <div style="width:160px; float:left;">
+      <div id="nav">тут будемо вставляти календар з бронюванням</div>
+    </div>
+ 
+  </main>
+  <div class="clear">
+  </div>
+ <footer>
+  <address>(с)Автор лабораторної роботи: студент спеціальності ПЗІС, Масенков Владислав Ігорович</address>
+ </footer>
+</body>
+</html>
